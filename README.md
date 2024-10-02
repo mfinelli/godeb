ca94c3cc001178583ac495a03a574f78350f8b5e
Mario Finelli <mario@finel.li>
2024-10-02 18:22:29 +0200

    Add initial changelog script

--- README.md
+++ README.md
@@ -4,3 +4,34 @@ I build [go](https://go.dev)-based services using arm64 and amd64 systems, but
 then I deploy them to (usually) raspberry pis and I prefer that to happen via
 debian packages. This script abstracts some of the common steps across my
 projects to simplify that process.
+
+## dependencies
+
+You must have the [yq](https://github.com/mikefarah/yq) utility available and
+in your path.
+
+You also need to have `git` available, and preferably, configured with a name
+and email address.
+
+## usage
+
+Create a file in your project `godeb.yaml` with the following contents:
+
+```yaml
+---
+# REQUIRED: used for various file and directory names
+project: myproj
+
+# REQUIRED: command to run to get the current project version
+version: jq -r .version package.json
+```
+
+### changelog
+
+We don't care about making a real changelog (this is an internal project not
+used for general consumption) however it's required for debian packaging so
+we have a script that can generate a fake, real changelog entry.
+
+```shell
+./changelog.bash noble # or whatever debian/ubuntu distribution want
+```
---
+++ changelog.bash
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+set -e
+
+# update the debian/changelog with the current version and date
+# usage: ./changelog.bash distribution
+# example: ./changelog.bash noble
+
+if [[ $# -ne 1 ]]; then
+  echo >&2 "usage: $(basename "$0") distribution"
+  exit 1
+fi
+
+if ! command -v yq > /dev/null 2>&1; then
+  echo >&2 "error: can not find yq in path!"
+  exit 1
+fi
+
+if [[ ! -f godeb.yaml ]]; then
+  echo >&2 "error: can't find godeb.yaml"
+  exit 1
+fi
+
+if [[ ! -d debian ]]; then
+  echo >&2 "error: can't find debian directory"
+  exit 1
+fi
+
+for setting in project version; do
+  if ! yq e -e ".${setting}" godeb.yaml > /dev/null 2>&1; then
+    echo >&2 "error: missing key $setting in godeb.yaml"
+    exit 1
+  fi
+done
+
+date="date"
+
+if [[ $(uname) == Darwin ]]; then
+  if ! command -v gdate > /dev/null 2>&1; then
+    echo >&2 "error: you must install gnu coreutils"
+    exit 1
+  fi
+
+  date="gdate"
+fi
+
+shortname="$1"
+project="$(yq e .project godeb.yaml)"
+versioncmd="$(yq e .version godeb.yaml)"
+version="$($versioncmd)"
+header="$project ($version) $shortname; urgency=medium"
+echo "$header" > debian/changelog
+echo >> debian/changelog
+
+subject=""
+msg=""
+if git show-ref --tags "$version" --quiet; then
+  subject="$(git tag -l --format='%(contents:subject)' "$version")"
+  msg="$(git tag -l --format='%(contents:body)' "$version")"
+  [[ -z $subject && -z $msg ]] && subject="Release version $version"
+elif git show-ref --tags "v$version" --quiet; then
+  subject="$(git tag -l --format='%(contents:subject)' "v$version")"
+  msg="$(git tag -l --format='%(contents:body)' "v$version")"
+  [[ -z $subject && -z $msg ]] && subject="Release version $version"
+else
+  subject="Release version $version"
+  msg=""
+fi
+
+body=""
+if [[ -z $msg ]]; then
+  body="  * $subject"
+else
+  # shellcheck disable=SC2001
+  body="$(sed 's/^/  /' <<< "$msg")"
+fi
+
+{
+  echo -n "$body"
+  echo
+} >> debian/changelog
+
+cdate="$($date -R)"
+user="$(git config user.name)"
+[[ -z $user ]] && user="$(whoami)"
+email="$(git config user.email)"
+[[ -z $email ]] && email="$(whoami)@$(hostname)"
+footer=" -- $user <$email>  $cdate"
+echo "$footer" >> debian/changelog
+
+exit 0
